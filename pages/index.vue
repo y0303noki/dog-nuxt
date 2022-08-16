d0c9779521c8e0d232d3f40d174b87ad8f08278a
yudate <y0303noki@gmail.com>
2022-08-16 22:20:06 +0900

    first

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="mx-20">
+      <div class="h-20"></div>
+      <div class="text-center text-4xl">今日の犬種は</div>
+      <div class="text-center text-4xl m-8">シェップランドシープドッグ</div>
+      <div class="grid grid-cols-3 gap-4">
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+        <img
+          src="https://images.dog.ceo/breeds/boxer/n02108089_2791.jpg"
+          class="w-52 h-52 object-cover"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
